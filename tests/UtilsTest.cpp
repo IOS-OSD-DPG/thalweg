519a883d9d9321381b47b23b6be322de106e4964
James Hannah <jhannah@protonmail.com>
2022-03-18 13:59:17 -0700

    add utils::split test

--- tests/UtilsTest.cpp
+++ tests/UtilsTest.cpp
@@ -2,7 +2,28 @@
 
 #include "doctest.h"
 
+#include <iostream>
+
 using namespace thalweg;
+
+// the standard forbids this, but it seems to work
+namespace std
+{
+template<typename T>
+auto operator<<(std::ostream& os, std::vector<T> const& vec) -> std::ostream&
+{
+	os << "[";
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		if (i < vec.size() - 1)
+			os << vec[i] << ", ";
+		else
+			os << vec[i];
+	}
+	os << "]";
+	return os;
+}
+}
 
 TEST_SUITE("UtilsTest")
 {
@@ -10,6 +31,13 @@ TEST_SUITE("UtilsTest")
 TEST_CASE("split returns single string when char not present")
 {
 	CHECK(utils::split("no newlines", '\n') == std::vector<std::string>{"no newlines"});
+}
+
+TEST_CASE("split returns two values when char appears once")
+{
+	CHECK(utils::split("easy-mode", '-') == std::vector<std::string>{"easy", "mode"});
+	CHECK(utils::split("-hardmode", '-') == std::vector<std::string>{"", "hardmode"});
+	CHECK(utils::split("hardmode-", '-') == std::vector<std::string>{"hardmode", ""});
 }
 
 }
