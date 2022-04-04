59d7b191ba4e33c555a9d531abb2e9296e7eb24f
James Hannah <jhannah@protonmail.com>
2022-04-04 12:41:35 -0700

    make section plot extend beyond minimum depth

--- section.py
+++ section.py
@@ -2,20 +2,25 @@ import argparse
 import csv
 import matplotlib.pyplot as plt
 
+def next_display_threshold(elevation):
+    fifties = int(elevation) // -50
+    return -50 * (fifties + 1)
+
 def main(inputfile):
     with open(inputfile) as csvfile:
         reader = csv.DictReader(csvfile)
         data = [(float(row["distance"]), -float(row["depth"])) for row in reader]
     x, y = zip(*data)
+    bottom = next_display_threshold(min(y))
 
     _, ax = plt.subplots()
     ax.plot(x, y, color="black")
-    ax.fill_between(x, y, min(y), color="grey")
+    ax.fill_between(x, y, bottom, color="grey")
 
     ax.set_xlabel("Distance (km)")
     ax.set_xlim([x[0], x[-1]])
     ax.set_ylabel("Depth (m)")
-    ax.set_ylim([min(y), 0])
+    ax.set_ylim([bottom, 0])
 
     plt.savefig(inputfile.replace(".csv", ".png"))
 
