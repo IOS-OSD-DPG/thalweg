01e88b25326a25bbf9be2e598e2c8a36991a5ab8
James Hannah <jhannah@protonmail.com>
2022-04-11 10:50:52 -0700

    make depth labels appear positive while keeping direction

--- section-map.py
+++ section-map.py
@@ -2,9 +2,11 @@ import argparse
 import csv
 import matplotlib.pyplot as plt
 
+THRESHOLD = 50
+
 def next_display_threshold(elevation):
-    fifties = int(elevation) // -50
-    return -50 * (fifties + 1)
+    fifties = int(elevation) // -THRESHOLD
+    return -THRESHOLD * (fifties + 1)
 
 def main(inputfile):
     with open(inputfile) as csvfile:
@@ -16,6 +18,15 @@ def main(inputfile):
     _, ax = plt.subplots()
     ax.plot(x, y, color="black")
     ax.fill_between(x, y, bottom, color="grey")
+
+    # bottom is negative, steps must be positive
+    # +1 so that the last value is included
+    steps = -bottom // THRESHOLD + 1
+    ax.set_yticks(
+        # labels must go negative
+        [n * THRESHOLD * -1 for n in range(steps)],
+        [n * THRESHOLD for n in range(steps)]
+    )
 
     ax.set_xlabel("Distance (km)")
     ax.set_xlim([x[0], x[-1]])
